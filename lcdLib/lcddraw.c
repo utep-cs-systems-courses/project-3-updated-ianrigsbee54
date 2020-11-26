b1a3ef11c374596a19dbc73a8b092d7c46f09ef0
ianrigsbee54 <irrigsbee@miners.utep.edu>
2020-11-26 14:19:51 -0700

    implemented drawChar8x12 and drawString8x12 in both lcddraw.c and lcddraw.h

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -71,6 +71,27 @@ void drawChar5x7(u_char rcol, u_char rrow, char c,
     row++;
   }
 }
+/*basically the drawChar5x7 function except modified for the 8x12 font*/
+void drawChar8x12(u_char rcol, u_char rrow, char c,
+		  u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char col = 0;
+  u_char row = 0;
+  u_char bit = 0x01;
+  u_char oc = c - 0x20;
+
+  lcd_setArea(rcol, rrow, rcol + 7, rcol + 12);
+  while(row < 13){
+    while(col < 8){
+      u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
+      lcd_writeColor(colorBGR);
+      col++;
+    }
+    col = 0;
+    bit <<= 1;
+    row++;
+  }
+}
 
 /** Draw string at col,row
  *  Type:
@@ -94,6 +115,16 @@ void drawString5x7(u_char col, u_char row, char *string,
   }
 }
 
+/*modified version of the drawString5x7 function used for the 8x12 font instead*/
+void drawString8x12(u_char col, u_char row, char *string,
+		    u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char cols = col;
+  while(*string) {
+    drawChar8x12(cols, row, *string++, fgColorBGR, bgColorBGR);
+    cols+=9;
+  }
+}
 
 /** Draw rectangle outline
  *  
